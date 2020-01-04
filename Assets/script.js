590bae8eb32e7a3f25de027094a8f411f6f02eaf
AndrewL <lawna12@gmail.com>
2020-01-04 13:19:28 -0500

    assets, js

--- Assets/script.js
+++ Assets/script.js
@@ -3,7 +3,7 @@ var APIKey = "1a6f238b-106c-4d68-9c6e-c3629e6c9a00"
 var APIquery = "cars"
 
 $.ajax({
-    url: articleURL + APIquery + APIKey
+    url: articleURL + APIquery + APIKey,
     method: "GET"
 }).then(function(response){
     console.log(response)
