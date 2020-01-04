de8095f57902c238bb994a345924feed31212cbd
AndrewL <lawna12@gmail.com>
2020-01-04 13:49:44 -0500

    added input vars

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,11 @@
 
-var APIKey = "VAPqVHTn1B0azfCnmll5YUg2TIsvHlHR";
-var APIquery = $('#searchP')
-var articleURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + APIquery + "&api-key=" + APIKey;
+var apiKey = "VAPqVHTn1B0azfCnmll5YUg2TIsvHlHR";
+var searchAPI = $('#searchP')
+var numRecords = $('#numRecords').value
+var startYear = $('#startYear')
+var articleURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchAPI + "&api-key=" + apiKey;
 
-
+console.log(numRecords)
 $.ajax({
     url: articleURL,
     method: "GET"
